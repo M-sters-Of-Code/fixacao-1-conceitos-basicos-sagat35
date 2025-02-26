bf80f571594a41a75d3408c7626b222df565443c
sagat35 <guilhermme_10@hotmail.com>
2025-02-26 20:11:35 -0300

    Exercicio 2 feito

---
+++ FixacaoConceitosBasicos1/Model/MetrosMilimetros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoConceitosBasicos1.Model
+{
+    internal class MetrosMilimetros
+    {
+    
+    public static void Main(string[] args)
+    {
+        int metro = 0;
+        int milimetro = 0;
+        Console.WriteLine("Digite o valor em Metros:");
+        metro = Convert.ToInt16(Console.ReadLine());
+        milimetro = metro * 1000;
+        Console.WriteLine(resultado_mm);
+        Console.WriteLine("\nEspero que tenha gostado!");
+
+    }
+}
+}
